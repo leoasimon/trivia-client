7841ae19f52cd9b72f5d37999e385ed60c27eea2
lsimon <leo@lsimon.org>
2022-11-26 13:30:12 +0100

    layout: minimal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Divider, Space, Typography } from "antd";
 import "./App.css";
+import GameMenu from "./components/GameMenu";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <Space direction="vertical" className="app-body">
+      <Space className="app-header">
+        <Typography.Title level={3}>Trivia</Typography.Title>
+      </Space>
+      <Divider />
+      <Space className="app-content">
+        <GameMenu />
+      </Space>
+    </Space>
+  );
 }
 
 export default App;
---
+++ src/components/GameMenu/index.tsx
@@ -0,0 +1,13 @@
+import { Button, List } from "antd";
+
+const GameMenu: React.FC = () => {
+  return (
+    <List>
+      <List.Item>
+        <Button>Single player</Button>
+      </List.Item>
+    </List>
+  );
+};
+
+export default GameMenu;
